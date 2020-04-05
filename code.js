369d1c6e11d464d9cd888e5cb5b861619d50f766
zjyhutka <63121278+zjyhutka@users.noreply.github.com>
2020-04-05 20:56:29 +0300

    Update code.js

--- code.js
+++ code.js
@@ -4,13 +4,23 @@ var ctx = canva.getContext('2d');
 //for KuraImage
 var imgOfKura = new Image();
 imgOfKura.src = "kura.png";
+imgOfBirch.src = "ГОТ_берёза.png";
 imgOfKura.addEventListener("load", function(e){
     var width = imgOfKura.naturalWidth;
     var height = imgOfKura.naturalHeight;
     canva.height = height/2;
     canva.width = width/2;
     ctx.drawImage(imgOfKura, 0, 0, canva.width, canva.height);
-    canva.addEventListener("click", count);
+    //canva.addEventListener("click", count);
+}, false);
+
+imgOfBirch.addEventListener("load", function(e){
+    var width = imgOfBirch.naturalWidth;
+    var height = imgOfBirch.naturalHeight;
+    canva.height = height/3;
+    canva.width = width/3;
+    ctx.drawImage(imgOfBirch, 0, 0, canva.width, canva.height);
+    imgOfBirch.addEventListener("click", count);
 }, false);
 
 //for counter
