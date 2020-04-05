296a71119ddeed786006b8e08bf53e73655fe591
alinefrax <41974250+alinefrax@users.noreply.github.com>
2020-04-05 23:01:41 +0300

    Update code.js

--- code.js
+++ code.js
@@ -19,10 +19,11 @@ imgOfKura.addEventListener("load", function(e){
 var imgOfKura = new Image();
 var imgOfBirch = new Image();
 var imgOfPol = new Image();
+var gif = new Image();
 imgOfKura.src = "kura.png";
 imgOfBirch.src = "ГОТ_берёза.png";
 imgOfPol.src = "ГОТ_полянка.png";
-
+gif.src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.pinterest.com%2Fpin%2F598345500467362823%2F&psig=AOvVaw3B6JwFgbcQr2jOJorBhYWP&ust=1586203161937000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCMCS9rKJ0ugCFQAAAAAdAAAAABAD"
 imgOfPol.addEventListener("load", function(e){
     var width = imgOfPol.naturalWidth;
     var height = imgOfPol.naturalHeight;
@@ -41,6 +42,13 @@ imgOfBirch.addEventListener("load", function(e){
     var width = imgOfBirch.naturalWidth;
     var height = imgOfBirch.naturalHeight;
     ctx.drawImage(imgOfBirch, 334, 74, width/6, height/6);
+    //canva.addEventListener("click", count);
+}, false);
+
+gif.addEventListener("load", function(e){
+    var width = gif.naturalWidth;
+    var height = gif.naturalHeight;
+    ctx.drawImage(gif, 600, 100, width, height);
     //canva.addEventListener("click", count);
 }, false);
 
