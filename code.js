aff056510933ed75331dd4e0435f6ef071c322ed
alinefrax <41974250+alinefrax@users.noreply.github.com>
2020-04-05 16:58:28 +0300

    Update code.js

--- code.js
+++ code.js
@@ -1,3 +1,20 @@
+var canva = document.getElementById('game');
+var ctx = canva.getContext('2d');
+
+//for KuraImage
+var imgOfKura = new Image();
+imgOfKura.src = "kura.png";
+imgOfKura.addEventListener("load", function(e){
+    var width = imgOfKura.naturalWidth;
+    var height = imgOfKura.naturalHeight;
+    canva.height = height/3;
+    canva.width = width/3;
+    ctx.drawImage(imgOfKura, 0, 0, width/3, height/3);
+
+    imgOfKura.addEventListener(“mousedown”, count);
+}, false);
+
+//for counter
 let i = 0;
 let OurPointer = true;
 document.getElementById("demo").innerHTML = i;
